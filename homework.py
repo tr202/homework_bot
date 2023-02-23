3822975044abe9a2949d2b8a9576e3a8c06786ed
tr202 <tr202@ya.ru>
2023-02-23 15:35:14 +0300

    del BOT

--- homework.py
+++ homework.py
@@ -17,7 +17,6 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-BOT = os.getenv('BOT')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
