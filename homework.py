7573c518ca1e20fcf4a142c885bed81a34a6d484
tr202 <tr202@ya.ru>
2023-02-23 15:31:56 +0300

    Update logic

--- homework.py
+++ homework.py
@@ -1,5 +1,4 @@
 """Проверяет статус домашней работы."""
-import json
 import logging
 import os
 import sys
@@ -37,6 +36,24 @@ TOKENS = {
     'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
 }
 
+REQUIRED_VALUES_NOT_FOUND = 'Отсутствуют обязательные переменные окружения'
+REQUIRED_VALUE_NOT_FOUND = ('Отсутствует обязательная'
+                            'переменная окружения {}')
+PROGRAM_STOPPED = 'Программа принудительно остановлена.'
+MESSAGE_SEND = 'Сообщение отправлено {}'
+MESSAGE_NOT_SEND = 'Не удалось отправить сообщение {}'
+API_ANSWER_CODE = 'Код ответа API {}'
+WRONG_JSON = 'Неверный формат Json'
+WRONG_ANSWER_PRACTICUM_API = 'Ошибка ответа сервера API практикум'
+WRONG_FORMAT_PRACTICUM_API = 'Неверный формат ответа API практикум'
+HOMEWORKS_KEY_NOT_EXISTS = 'Отсутствует ключ homeworks'
+UNEXPECTED_HOMEWORK_STATUS = 'Неожиданный статус домашней работы {}'
+HOMEWORKS_NAME_KEY_NOT_EXISTS = 'Отсутсвует ключ homework_name'
+STATUS_CHANGE = 'Изменился статус проверки работы \"{}\".\n{}'
+STATUS_NOT_CHANGE = 'Статус не изменился'
+UNEXPECTED_DATE_FORMAT = 'Неверный формат даты'
+PROGRAM_ERROR = 'Сбой в работе программы: {error}'
+
 this_bot = {}
 
 logging.basicConfig(
@@ -68,33 +85,20 @@ def check_tokens():
     tokens = (TELEGRAM_CHAT_ID, PRACTICUM_TOKEN, TELEGRAM_TOKEN)
     if all(tokens):
         return True
-    logger.critical('Отсутствуют обязательные переменные окружения')
+    logger.critical(REQUIRED_VALUES_NOT_FOUND)
     for key, value in TOKENS.items():
         if not value:
-            logger.critical(
-                f'Отсутствует обязательная переменная окружения {key}')
-    raise Exception('Программа принудительно остановлена.')
+            logger.critical(REQUIRED_VALUE_NOT_FOUND.format(key))
+    raise Exception(PROGRAM_STOPPED)
 
 
 def send_message(bot, message):
     """Отправляет сообщение в чат телеграм."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger.debug(f'Сообщение отправлено {message}')
+        logger.debug(MESSAGE_SEND.format(message))
     except Exception:
-        logger.error(f'Не удалось отправить сообщение {message}')
-
-
-def check_json(response):
-    """Проверяет формат Json."""
-    try:
-        if __name__ == '__main__':
-            json.loads(response)
-        send_once('json_error')
-    except json.JSONDecodeError as error:
-        if send_once('json_error', str(error), True):
-            this_bot.get('bot')('Неверный формат Json')
-        logger.error('Неверный формат Json')
+        logger.error(MESSAGE_NOT_SEND.format(message))
 
 
 def get_api_answer(timestamp):
@@ -107,35 +111,37 @@ def get_api_answer(timestamp):
         )
         status_code = response.status_code
         if status_code != HTTPStatus.OK:
-            this_bot.get('bot')(f'Код ответа API {status_code}')
+            this_bot.get('bot')(API_ANSWER_CODE.format(status_code))
             raise Exception
         else:
             send_once('request_error')
-
-            json_data = response.json()
-            check_json(response.text)
-            if isinstance(json_data, dict):
-                if isinstance(json_data['homeworks'], list):
-                    return json_data
+            try:
+                json_data = response.json()
+                send_once('json_error')
+            except Exception as error:
+                if send_once('json_error', str(error), True):
+                    this_bot.get('bot')(WRONG_JSON)
+                logger.error(WRONG_JSON)
+            return json_data
     except requests.RequestException as request_error:
         if send_once('request_error', request_error, True):
-            this_bot.get('bot')('Ошибка ответа сервера API практикум')
-        logger.error('Ошибка ответа сервера API практикум')
+            this_bot.get('bot')(WRONG_ANSWER_PRACTICUM_API)
+        logger.error(WRONG_ANSWER_PRACTICUM_API)
 
 
 def check_response(response):
     """Проверяет ответ API на соответствие документации."""
     if not isinstance(response, dict):
-        this_bot.get('bot')('Неверный формат ответа')
-        logger.error('Неверный формат ответа API практикум')
+        this_bot.get('bot')(WRONG_FORMAT_PRACTICUM_API)
+        logger.error(WRONG_FORMAT_PRACTICUM_API)
         raise TypeError
     if 'homeworks' not in response:
-        this_bot.get('bot')('Отсутствует ключ homeworks')
-        logger.error('Отсутствует ключ homeworks')
+        this_bot.get('bot')(HOMEWORKS_KEY_NOT_EXISTS)
+        logger.error(HOMEWORKS_KEY_NOT_EXISTS)
         raise Exception
     if not isinstance(response.get('homeworks'), list):
-        this_bot.get('bot')('Неверный формат ответа')
-        logger.error('Неверный формат ответа API практикумю')
+        this_bot.get('bot')(WRONG_FORMAT_PRACTICUM_API)
+        logger.error(WRONG_FORMAT_PRACTICUM_API)
         raise TypeError
     return True
 
@@ -144,26 +150,27 @@ def parse_status(homework):
     """Возвращает статус проверки домашней работы."""
     verdict = homework.get('status')
     if verdict not in HOMEWORK_VERDICTS.keys():
-        this_bot.get('bot')(f'Неожиданный статус домашней работы {verdict}')
-        logger.error(f'Неожиданный статус домашней работы {verdict}')
+        this_bot.get('bot')(UNEXPECTED_HOMEWORK_STATUS.format(verdict))
+        logger.error(UNEXPECTED_HOMEWORK_STATUS.format(verdict))
         raise UnexpectedStatus
     if 'homework_name' not in homework:
-        this_bot.get('bot')('Отсутсвует ключ homework_name')
-        logger.error('Отсутсвует ключ homework_name')
+        this_bot.get('bot')(HOMEWORKS_NAME_KEY_NOT_EXISTS)
+        logger.error(HOMEWORKS_NAME_KEY_NOT_EXISTS)
         raise Exception
     homework_name = homework.get('homework_name')
     if os.environ.get('verdict') != verdict:
         os.environ['verdict'] = verdict
         homework_name = homework.get('homework_name')
-        return (f'Изменился статус проверки работы "{homework_name}".'
-                f'{verdict}\n{HOMEWORK_VERDICTS.get(verdict)}')
-    logger.debug('Статус не изменился')
+        return (
+            STATUS_CHANGE.format(
+                homework_name, HOMEWORK_VERDICTS.get(verdict)))
+    logger.debug(STATUS_NOT_CHANGE)
     return
 
 
 def check_bot(bot):
     """Проверяет что бот запустился."""
-    if str(bot) == BOT:
+    if bot.get_me().is_bot:
         return True
     else:
         raise Exception
@@ -174,9 +181,9 @@ def check_timestamp(timestamp):
     if isinstance(timestamp, int) and datetime.fromtimestamp(timestamp):
         send_once('date_error')
         return
-    if send_once('date_error', 'Неверный формат даты', True):
-        this_bot.get('bot')('Неверный формат даты')
-        logger.error('Неверный формат даты')
+    if send_once('date_error', UNEXPECTED_DATE_FORMAT, True):
+        this_bot.get('bot')(UNEXPECTED_DATE_FORMAT)
+        logger.error(UNEXPECTED_DATE_FORMAT)
     return True
 
 
@@ -205,7 +212,7 @@ def main():
                 if status_message:
                     send_message(bot, status_message)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = PROGRAM_ERROR.format(error)
             send_message(bot, message)
             logger.error(message)
             raise error
